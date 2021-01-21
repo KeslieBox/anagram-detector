5246e57db1bc717281f005fee4b80bb68c472a55
KeslieBox <kesliebox@gmail.com>
2021-01-20 18:23:35 -0600

    commit tree 6ee8b2f727a6e68f8f5bcf7da3ef7c11ceb59054

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    attr_accessor :initializer_word
+
+    def initialize(initializer_word)
+        @initializer_word = initializer_word
+    end
+
+    def match(word)
+        word_array = []
+        word_sorted = ""
+        initializer_sorted = ""
+        # iterate over the array of words that .match method takes as args 
+        # compare each word of that array to the initializer_word
+        word.each do |word1|
+            puts word
+            # binding.pry
+        # use #split and #sort to compare arrays and see if they contain the same letters
+            word_sorted = word1.split("").sort.join("")
+            initializer_sorted = @initializer_word.split("").sort.join("")
+            # binding.pry  
+            if word_sorted == initializer_sorted
+                word_array << word1
+            end   
+        end
+        word_array  
+    end
+end
+
+
+
